70406450ce2b2396727387647cdbb4739e0111e6
Feijoa <suleymanov5471@gmail.com>
2025-10-11 10:37:09 +0500

    Написал  функцию для задачи "Делу-время"

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,24 @@
+const hoursInMinutes = (time) => {
+  const [hours, minutes] = time.split(':');
+  return parseInt(hours, 10) * 60 + parseInt(minutes, 10);
+};
+
+const isDuringBusinessHours = (startTime, endTime, startMeeting, durationMeeting) => {
+  const startTimeInMinute = hoursInMinutes(startTime);
+  const endTimeInMinute = hoursInMinutes(endTime);
+  const startMeetingInMinute = hoursInMinutes(startMeeting);
+  const endMeetingInMinute = startMeetingInMinute + durationMeeting;
+  return (endTimeInMinute - endMeetingInMinute >= 0) && (startMeetingInMinute - startTimeInMinute >= 0);
+};
+
+isDuringBusinessHours('08:00', '17:30', '14:00', 90); // true
+isDuringBusinessHours('8:0', '10:0', '8:0', 120);     // true
+isDuringBusinessHours('08:00', '14:30', '14:00', 90); // false
+isDuringBusinessHours('14:00', '17:30', '08:0', 90);  // false
+isDuringBusinessHours('8:00', '17:30', '08:00', 900); // false
+
+
+/*
 //Проверка длины строки
 function checkStringLength(srtingForCheck, maxLength) {
   return (srtingForCheck.length <= maxLength);
@@ -49,3 +70,7 @@ getNumberFromString('a я томат');
 getNumberFromString(2023);
 getNumberFromString(-1);
 getNumberFromString(1.5);
+
+*/
+
+
